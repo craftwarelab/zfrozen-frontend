195703328666a9bf8786df7fa20a2b5bc6f1c877
nipun_madusanka <jmnipun@outlook.com>
2024-10-09 23:27:26 +0530

    up

--- src/views/home/Night.js
+++ src/views/home/Night.js
@@ -33,7 +33,7 @@ const Night = () => {
             deliver exceptional results.
           </p>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        <div>
           <TeamGrid data={teamGrid} />
         </div>
       </div>
