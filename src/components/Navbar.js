98f432af6cdffe2c8caa812c91bca184ec53a137
DakshithaDissanayaka <dakshithadissanayaka96@gmail.com>
2024-12-05 18:40:18 +0530

    fix wdith of footer and header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
 
   return (
     <nav className="bg-white shadow-md sticky top-0 z-50">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-auto mx-36 px-4 sm:px-6 lg:px-4">
         <div className="flex justify-between h-16 ">
           <div className="flex items-center">
             {/* Logo */}
