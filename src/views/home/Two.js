f61a02733daf1a577511ed51b610154747782e5f
DakshithaDissanayaka <dakshithadissanayaka96@gmail.com>
2024-12-07 17:49:33 +0530

    update

--- src/views/home/Two.js
+++ src/views/home/Two.js
@@ -1,41 +1,27 @@
 import React from "react";
-import {
-  // FaRegClock,
-  // FaUsers,
-  // FaBriefcase,
-  // FaCcVisa,
-  // FaCcPaypal,
-  // FaCcMastercard,
-  // FaCcJcb,
-  // FaCcAmazonPay,
-} from "react-icons/fa6";
 
 const Two = () => {
-  // const companyIcons = [
-  //   <FaCcVisa key="visa" className="text-[55px]" />,
-  //   <FaCcPaypal key="paypal" className="text-[55px]" />,
-  //   <FaCcMastercard key="mastercard" className="text-[55px]" />,
-  //   <FaCcJcb key="jcb" className="text-[55px]" />,
-  //   <FaCcAmazonPay key="amazon" className="text-[55px]" />,
-  // ];
   return (
-    <div className="h-full select-none">
-      <div className="grid grid-rows-2 ">
-        <div className="h-full sm:h-[34rem] lg:h-[30rem] relative overflow-hidden">
+    <div className="select-none">
+      <div className="grid grid-rows-2">
+        {/* First Section */}
+        <div className="h-[33rem] sm:h-[38rem] lg:h-[30rem] relative overflow-hidden">
           <img
             src="/fc1a3c66d4b0d86a0f3fa2ab7bb672f0.png"
             alt="hello"
-            className="h-full top-0 right-0 object-cover object-center"
+            className="h-full w-full top-0 right-0 object-cover object-center"
           />
           <div className="absolute top-0 left-0 w-full h-full px-4 mx-auto sm:px-6 lg:px-8 z-30 mt-12">
             <div className="px-auto xl:px-32">
+              {/* About Our Company Button */}
               <div className="flex justify-center items-center sm:justify-start">
-                <button className=" text-[#000000] text-[28px] font-[500] px-8 py-2 rounded-[28px] bg-[#FFD700] cursor-pointer hover:bg-[#ffe240]">
+                <button className="text-[#000000] text-[20px] sm:text-[28px] font-[500] px-6 py-2 sm:px-8 sm:py-2 rounded-[28px] bg-[#FFD700] cursor-pointer hover:bg-[#ffe240]">
                   About Our Company
                 </button>
               </div>
-              <div className="mt-12">
-                <p className="text-[#FFFFFF] text-[18px] font-[500] text-center sm:text-start">
+              {/* Company Description */}
+              <div className="mt-8 sm:mt-12">
+                <p className="text-[#FFFFFF] text-[14px] sm:text-[18px] font-[500] text-center sm:text-start leading-relaxed">
                   &quot;Zfrozen is a dynamic team of graduates from South
                   Eastern University, Sri Lanka, united by a common passion for
                   IT-related projects. Established in October 2022, our journey
@@ -46,8 +32,9 @@ const Two = () => {
                   push boundaries and shape the future of technology.&quot;
                 </p>
               </div>
-              <div className="mt-[2.8rem] sm:mt-[1.8rem] md:mt-[2rem] lg:mt-28">
-                <h1 className="text-white text-center sm:text-start text-[40px] lg:text-[50px] font-[700]">
+              {/* Why Hire Us */}
+              <div className="mt-8 sm:mt-16">
+                <h1 className="text-white text-center sm:text-start text-[28px] sm:text-[40px] lg:text-[50px] font-[700]">
                   Why <span className="text-[#FFD700]">Hire us</span>?
                 </h1>
               </div>
@@ -55,88 +42,77 @@ const Two = () => {
           </div>
         </div>
 
-        <div className="h-full px-8 sm:px-14 relative">
-          {/* <div className="relative"> */}
-          <div className="absolute top-0 left-0 -translate-y-[8.7rem] w-full h-full flex flex-col items-center md:items-end md:px-32">
-            <div className="p-5 md:p-10 bg-[#FFD700] rounded-[20px]">
-              <div className="flex flex-row gap-6 justify-between items-center font-[600]">
+        {/* Second Section */}
+        <div className="h-full px-4 sm:px-14 relative">
+          {/* Card Section */}
+          <div className="absolute top-0 left-0 -translate-y-[6rem] sm:-translate-y-[8.7rem] w-full h-full flex flex-col items-center sm:items-end sm:px-10 md:px-32">
+            <div className="p-4 sm:p-10 bg-[#FFD700] rounded-[20px] w-[90%] sm:w-auto">
+              <div className="flex flex-row gap-4 sm:gap-6 justify-between items-center font-[600]">
+                {/* Completed Projects */}
                 <div className="flex flex-col justify-center items-center gap-2">
-                  <span>
-                    {/* <FaRegClock className="text-[28px] font-[700]" /> */}
-                    <img
-                      src="/completed-project.png"
-                      alt="test"
-                      className="top-0 right-0 object-cover object-center h-[2rem] md:h-[4rem] lg:h-[3rem]"
-                    />
-                  </span>
-                  <p className="font-[700]">35+</p>
-                  <p className="text-[8px] md:text-[12px] font-[600]">
-                    Completed Project
+                  <img
+                    src="/completed-project.png"
+                    alt="test"
+                    className="h-[2rem] sm:h-[4rem] lg:h-[3rem]"
+                  />
+                  <p className="font-[700] text-[16px] sm:text-[20px]">35+</p>
+                  <p className="text-[10px] sm:text-[12px] font-[600]">
+                    Completed Projects
                   </p>
                 </div>
 
+                {/* Happy Customers */}
                 <div className="flex flex-col justify-center items-center gap-2">
-                  <span>
-                    {/* <FaUsers className="text-[28px] font-[700]" /> */}
-                    <img
-                      src="/happy-customers.png"
-                      alt="test"
-                      className="top-0 right-0 object-cover object-center h-[2rem] md:h-[4rem] lg:h-[3rem]"
-                    />
-                  </span>
-                  <p className="font-[700]">25+</p>
-                  <p className="text-[8px] md:text-[12px] font-[600]">
+                  <img
+                    src="/happy-customers.png"
+                    alt="test"
+                    className="h-[2rem] sm:h-[4rem] lg:h-[3rem]"
+                  />
+                  <p className="font-[700] text-[16px] sm:text-[20px]">25+</p>
+                  <p className="text-[10px] sm:text-[12px] font-[600]">
                     Happy Customers
                   </p>
                 </div>
 
+                {/* Years of Experience */}
                 <div className="flex flex-col justify-center items-center gap-2">
-                  <span>
-                    {/* <FaBriefcase className="text-[28px] font-[700]" /> */}
-                    <img
-                      src="/years-of-experience.png"
-                      alt="test"
-                      className="top-0 right-0 object-cover object-center h-[2rem] md:h-[4rem] lg:h-[3rem]"
-                    />
-                  </span>
-                  <p className="font-[700]">2+</p>
-                  <p className="text-[8px] md:text-[12px] font-[600]">
+                  <img
+                    src="/years-of-experience.png"
+                    alt="test"
+                    className="h-[2rem] sm:h-[4rem] lg:h-[3rem]"
+                  />
+                  <p className="font-[700] text-[16px] sm:text-[20px]">2+</p>
+                  <p className="text-[10px] sm:text-[12px] font-[600]">
                     Years of Experience
                   </p>
                 </div>
               </div>
             </div>
           </div>
-          {/* </div> */}
 
-          <div className="mt-32 sm:mt-44 md:mx-32">
-            <div className="flex justify-center items-center my-10">
-              <p className="text-[40px] font-[600] text-center">
+          {/* Clients Section */}
+          <div className="mt-10 sm:mt-44 md:mx-32">
+            <div className="flex justify-center items-center my-8">
+              <p className="text-[24px] sm:text-[40px] font-[600] text-center">
                 Amazing clients who trust us
               </p>
             </div>
-            <div className="flex flex-row justify-center items-center gap-4 sm:gap-16 py-6 overflow-x-auto ">
-              {/* {companyIcons.map((item, key) => (
-                <span className="icon" key={key}>
-                  {item}
-                </span>
-              ))} */}
+            <div className="flex flex-row justify-center items-center gap-4 sm:gap-16 py-6 overflow-x-auto">
               <img
                 src="/evc-logo-white.png"
                 alt="logo"
-                className="top-0 right-0 object-cover object-center h-[0rem] sm:h-[6rem] md:h-[8rem] lg:h-[10rem] select-none"
+                className="h-[4rem] sm:h-[6rem] md:h-[8rem] lg:h-[10rem] select-none"
                 draggable="false"
               />
               <img
                 src="/apple-center-logo-white.png"
                 alt="logo"
-                className="top-0 right-0 object-cover object-center h-[0rem] sm:h-[6rem] md:h-[8rem] lg:h-[10rem] select-none"
+                className="h-[4rem] sm:h-[6rem] md:h-[8rem] lg:h-[10rem] select-none"
                 draggable="false"
               />
             </div>
           </div>
         </div>
-
       </div>
     </div>
   );
