6078f8a49b7454122deb876d2b70bf979fb1c7ae
DakshithaDissanayaka <dakshithadissanayaka96@gmail.com>
2024-12-07 15:38:02 +0530

    update Welcome message

--- src/views/home/Three.js
+++ src/views/home/Three.js
@@ -1,47 +1,47 @@
-import React from "react";
+import React from 'react';
 
-const Three = () => {
+function App() {
   return (
-    <div className="h-full px-4 mx-auto sm:px-6 lg:px-8 bg-black w-full select-none">
-      <div className="xl:px-32">
-        <div className="grid grid-cols-3 h-full">
-          <div className="col-span-2 my-20">
+    <div className="h-fit bg-black select-none">
+
+
+      {/* Mobile Layout */}
+      <div className="grid lg:grid-cols-2 lg:grid-rows-1 md:grid-cols-2 sm:grid-cols-1 gap-4 min-h-fit px-4 mx-auto sm:px-6 lg:px-8">
+        {/* Left Column (Top) */}
+        <div className="grid grid-rows-3p-4 xl:px-32 my-20">
+          <div className="p-4">
             <h1 className="text-[45px] lg:text-[60px] font-[700] text-white text-wrap">
               Welcome message
             </h1>
-            <div className="mt-16 w-[12rem] md:w-auto lg:w-[40rem]">
-              <p className="text-white text-[18px] flex-wrap">
-                Welcome to Zfrozen, where innovation meets creativity.
-                We&lsquo;re passionate about shaping the future of IT with brilliance and dedication.
-                At Zfrozen, every project is a canvas for collaboration and innovation,
-                turning challenges into opportunities and ideas into success.
-                Let&lsquo;s craft the future together!
-              </p>
-            </div>
+          </div>
+          <div className="p-4">
+            <p className="text-white text-[18px] flex-wrap">
+              Welcome to Zfrozen, where innovation meets creativity.
+              We&lsquo;re passionate about shaping the future of IT with brilliance and dedication.
+              At Zfrozen, every project is a canvas for collaboration and innovation,
+              turning challenges into opportunities and ideas into success.
+              Let&lsquo;s craft the future together!
+            </p>
+          </div>
+          <div className="p-4">
             <h1 className="text-[45px] lg:text-[25px] font-[700] text-white text-wrap mt-16">
               Zfrozen Team
             </h1>
           </div>
+        </div>
 
+        {/* Right Column (Bottom) */}
+        <div className=" xl:px-32 ">
           <img
-            src="/zfrozen-logo-about.png"
+            src="/welcomlogo.png"
             alt="test"
-            className=" absolute md:h-[15rem] translate-y-[10rem] translate-x-[70rem] z-10"
-            draggable="false"
-          />
-
-          <div className="bg-[#FFD700] md:w-[15rem] translate-x-[10rem] relative z-0"></div>
-          <img
-            src="/texter.png"
-            alt="test"
-            className="absolute h-[23rem] translate-y-[0rem] translate-x-[62.5rem]"
+            className=" md:h-[35rem]"
             draggable="false"
           />
         </div>
-        {/* <p className="text-white">test</p> */}
       </div>
     </div>
   );
-};
+}
 
-export default Three;
+export default App;
