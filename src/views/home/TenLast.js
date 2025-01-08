29ba104473d12522d68f65861e7c7de0da9fe830
DakshithaDissanayaka <dakshithadissanayaka96@gmail.com>
2025-01-08 16:23:40 +0530

    update

--- src/views/home/TenLast.js
+++ src/views/home/TenLast.js
@@ -9,18 +9,29 @@ const TenLast = () => {
             <p className="text-black font-[600] lg:text-[50px] sm:text-[40px] text-[30px] text-center">
               Hey! Let&lsquo;s Talk
             </p>
-            <p className="text-center  lg:px-20">
-              "Ready to turn your ideas into extraordinary products? At ZFROZEN, we specialize in innovative solutions, from AI-driven systems to IoT applications."
+            <p className="text-center lg:px-20">
+              "Ready to turn your ideas into extraordinary products? We specialize in transforming concepts into innovative solutions that make an impact."
             </p>
             <div className="flex flex-col sm:flex-row justify-center items-center gap-2">
-              <button className="w-full sm:w-[180px] h-[50px] text-black bg-white rounded-xl p-2 hover:bg-black hover:text-white">
-                Send us a message
-              </button>
-              <button className="w-full sm:w-[180px] h-[50px] text-black bg-white rounded-xl p-2 hover:bg-black hover:text-white">
-                Schedule a call
-              </button>
+              <a 
+                href="https://wa.me/94771173022?text=Hi%20there%2C%20I%20would%20like%20to%20discuss%20my%20project."
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <button className="w-full sm:w-[180px] h-[50px] text-black bg-white rounded-xl p-2 hover:bg-black hover:text-white">
+                  Send us a message
+                </button>
+              </a>
+              <a 
+                href="https://wa.me/94771173022"
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <button className="w-full sm:w-[180px] h-[50px] text-black bg-white rounded-xl p-2 hover:bg-black hover:text-white">
+                  Schedule a call
+                </button>
+              </a>
             </div>
-
           </div>
         </div>
       </div>
